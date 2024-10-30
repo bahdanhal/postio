f96fb150eb67baf2ef22ae9e1cba3d4b64d88f01
root <root@DESKTOP-QKK7PT4.localdomain>
2024-10-30 01:16:45 +0100

    services creating fix

--- app/Site/Services/ServiceContainer.php
+++ app/Site/Services/ServiceContainer.php
@@ -20,38 +20,22 @@ use Postio\User\Persistence\UserRepository;
 
 final readonly class ServiceContainer
 {
-    private PDO $connection;
-    private Auth $auth;
-    private SerializeUser $serializeUser;
-    private UnserializeUser $unserializeUser;
-    private UserRepository $userRepository;
-    private SerializePost $serializePost;
-    private UnserializePost $unserializePost;
-    private PostRepository $postRepository;
-    private GetAllPosts $getAllPosts;
-    private DeletePostById $deletePostById;
-    private CreatePost $createPost;
-    private UpdatePost $updatePost;
-    private GetPostFromRequest $getPostFromRequest;
-
-    public function __construct()
+    public function __construct(
+        private PDO $connection,
+        private Auth $auth,
+        private SerializeUser $serializeUser,
+        private UnserializeUser $unserializeUser,
+        private UserRepository $userRepository,
+        private SerializePost $serializePost,
+        private UnserializePost $unserializePost,
+        private PostRepository $postRepository,
+        private GetAllPosts $getAllPosts,
+        private DeletePostById $deletePostById,
+        private CreatePost $createPost,
+        private UpdatePost $updatePost,
+        private GetPostFromRequest $getPostFromRequest,
+    )
     {
-        $this->unserializeUser = new UnserializeUser();
-        $this->serializeUser = new SerializeUser();
-        $this->connection = new PDO($_ENV["DB_DSN"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
-
-        $this->userRepository = new UserRepository($this->connection, $this->unserializeUser, $this->serializeUser);
-        $this->auth = new Auth(
-            $this->userRepository,
-        );
-        $this->serializePost = new SerializePost();
-        $this->unserializePost = new UnserializePost();
-        $this->postRepository = new PostRepository($this->connection, $this->unserializePost, $this->serializePost);
-        $this->getAllPosts = new GetAllPosts($this->postRepository);
-        $this->deletePostById = new DeletePostById($this->postRepository);
-        $this->createPost = new CreatePost($this->postRepository);
-        $this->updatePost = new UpdatePost($this->postRepository);
-        $this->getPostFromRequest = new GetPostFromRequest();
     }
 
     public function getAuth(): Auth
--- app/Site/Services/ServiceContainerFactory.php
+++ app/Site/Services/ServiceContainerFactory.php
@@ -4,10 +4,55 @@ declare(strict_types=1);
 
 namespace Postio\Site\Services;
 
+use Postio\Site\Auth\Auth;
+use Postio\User\Persistence\SerializeUser;
+use Postio\User\Persistence\UnserializeUser;
+use PDO;
+use Postio\Post\CreatePost;
+use Postio\Post\DeletePostById;
+use Postio\Post\GetAllPosts;
+use Postio\Post\GetPostFromRequest;
+use Postio\Post\Persistence\PostRepository;
+use Postio\Post\Persistence\SerializePost;
+use Postio\Post\Persistence\UnserializePost;
+use Postio\Post\UpdatePost;
+use Postio\User\Persistence\UserRepository;
+
 final readonly class ServiceContainerFactory
 {
     public function __invoke(): ServiceContainer
     {
-        return new ServiceContainer();
+        $unserializeUser = new UnserializeUser();
+        $serializeUser = new SerializeUser();
+        $connection = new PDO($_ENV["DB_DSN"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
+
+        $userRepository = new UserRepository($connection, $unserializeUser, $serializeUser);
+        $auth = new Auth(
+            $userRepository,
+        );
+        $serializePost = new SerializePost();
+        $unserializePost = new UnserializePost();
+        $postRepository = new PostRepository($connection, $unserializePost, $serializePost);
+        $getAllPosts = new GetAllPosts($postRepository);
+        $deletePostById = new DeletePostById($postRepository);
+        $createPost = new CreatePost($postRepository);
+        $updatePost = new UpdatePost($postRepository);
+        $getPostFromRequest = new GetPostFromRequest();
+
+        return new ServiceContainer(
+            $connection,
+            $auth,
+            $serializeUser,
+            $unserializeUser,
+            $userRepository,
+            $serializePost,
+            $unserializePost,
+            $postRepository,
+            $getAllPosts,
+            $deletePostById,
+            $createPost,
+            $updatePost,
+            $getPostFromRequest,    
+        );
     }
 }
